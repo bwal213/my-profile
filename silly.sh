a81b57fc2b433f01a8fffe0779b20a6aa87b661d
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-09-12 22:54:26 -0400

    Initial update to silly.py for apache install.
    Updated silly.py to include all code that should install and then run apache2 on CentOS7. Ports opened as well for default and ssl access.
    
    Resources used:
    https://www.liquidweb.com/kb/how-to-install-apache-on-centos-7/

--- silly.sh
+++ silly.sh
@@ -2,3 +2,12 @@
 
 echo "This is a silly script" > /tmp/silly.txt
 
+sudo yum clean all
+sudo yum -y update
+sudo yum -y install httpd
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
+sudo systemctl stop httpd.service
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
